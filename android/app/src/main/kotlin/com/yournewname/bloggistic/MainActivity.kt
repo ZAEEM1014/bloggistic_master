a8691b9672b8e3e0c26774d8c94788b92c7c81b7
ZAEEM1014 <zaeemakh117@gmail.com>
2025-02-21 18:07:53 +0500

    Updated README and project settings

---
+++ android/app/src/main/kotlin/com/yournewname/bloggistic/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.yournewname.bloggistic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/widgets/bottom/desktop_bottom.dart
+++ lib/widgets/bottom/desktop_bottom.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../utils/colors.dart';
